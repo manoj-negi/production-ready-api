b53ef5c4cf4831db3d4ecbe3a8158775d4b21ca2
manoj-negi <www.msnegi@gmail.com>
2023-03-14 23:49:56 +0530

    db config

--- go.mod
+++ go.mod
@@ -1,5 +1,13 @@
-module github.com/manoj-negi/production-ready-api~
+module github.com/manoj-negi/production-ready-api
 
 go 1.19
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/jinzhu/gorm v1.9.16
+)
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/lib/pq v1.1.1 // indirect
+)
---
+++ internal/database/database.go
@@ -0,0 +1,34 @@
+package database
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+func NewDatabase() (*gorm.DB, error) {
+
+	dbUsername := os.Getenv("DB_USERNAME")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbDatabaseName := os.Getenv("DB_DATABASE_NAME")
+	dbPort := os.Getenv("DB_PORT")
+
+	// dbConnectionString := dbUsername + ":" + dbPassword + "@tcp(" + dbHost + ":" + strconv.Itoa(dbPort) + ")/" + dbDatabaseName
+	connectionString := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable", dbHost, dbPort, dbUsername, dbDatabaseName, dbPassword)
+	fmt.Println(connectionString)
+
+	db, err := gorm.Open("postgres", connectionString)
+	if err != nil {
+		return db, err
+	}
+
+	if err := db.DB().Ping(); err != nil {
+		return db, err
+	}
+
+	return db, nil
+
+}
